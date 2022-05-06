8a5ba57eb986ed5ddace676e608e5ae1432c2344
andreystepan <92333697+andreystepan@users.noreply.github.com>
2022-05-06 12:47:44 +0300

    copy

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,23 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const imageConteiner = document.querySelector('.gallery');
+const imageMarkup = creatGalleryItemsMarkup(galleryItems);
+
+
+imageConteiner.insertAdjacentHTML('beforeend', imageMarkup);
+
+
+function creatGalleryItemsMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+    <img class = "gallery__image" src="${preview}"
+    alt = "${description}" /></a>
+  `
+    }).join('');
+    
+}
+
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250, });
