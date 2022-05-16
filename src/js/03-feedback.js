67f85aaab6dd17dc506e19cbb4fa7ed8682202ac
andreystepan <92333697+andreystepan@users.noreply.github.com>
2022-05-16 22:18:23 +0300

    homework08(task03)

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,41 +2,41 @@
 const throttle = require('lodash.throttle');
 
 const form = document.querySelector(".feedback-form");
-const textarea = document.querySelector('textarea');
-const input = document.querySelector('input')
+const textarea = document.querySelector(".feedback-form textarea");
+const input = document.querySelector(".feedback-form input");
+let formData = {};
+const STORAGE_KEY = 'feedback-smg';
 
 form.addEventListener('submit', handleFormSubmit);
 form.addEventListener('input', throttle(handleFormDataInput,500));
 
-
-const STORAGE_KEY = 'feedback-smg';
 saveTextareaText();
-const formData = {};
-
-
 
 function handleFormSubmit(e) {
     e.preventDefault();
-
-    e.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KEY);
+    if (input.value !== '' && textarea.value !== '') {
+        console.log(formData);
+        e.target.reset();
+        formData = {};
+        localStorage.removeItem(STORAGE_KEY);
+    }
 };
  
-
-function handleFormDataInput(e) {
-    formData[e.target.name] = e.target.value;
-    console.log(formData);
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-
-}
 
 function saveTextareaText() {
     
     const saveMessage = localStorage.getItem(STORAGE_KEY);
     const parseSaveMessage = JSON.parse(saveMessage);
 
-    if (saveMessage) {
-        textarea.value = parseSaveMessage.message;
+    if (parseSaveMessage.email) {
         input.value = parseSaveMessage.email;
-    }
+    };
+    if (parseSaveMessage.message){
+        textarea.value = parseSaveMessage.message;  
+    };
 }
+function handleFormDataInput(e) {
+    formData[e.target.name] = e.target.value;
+    
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
