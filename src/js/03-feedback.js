c1244153c1e8f679cd16db78bcabe13bb3c9dd9f
andreystepan <92333697+andreystepan@users.noreply.github.com>
2022-05-18 22:02:06 +0300

    comitHW03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,6 +5,7 @@ const form = document.querySelector(".feedback-form");
 const textarea = document.querySelector(".feedback-form textarea");
 const input = document.querySelector(".feedback-form input");
 let formData = {};
+let resultStorage = {};
 const STORAGE_KEY = 'feedback-smg';
 
 form.addEventListener('submit', handleFormSubmit);
@@ -14,8 +15,9 @@ saveTextareaText();
 
 function handleFormSubmit(e) {
     e.preventDefault();
+    
     if (input.value !== '' && textarea.value !== '') {
-        console.log(formData);
+        console.log(resultStorage);
         e.target.reset();
         formData = {};
         localStorage.removeItem(STORAGE_KEY);
